87ccc396b262068c36c047de92a6d4cb5ee1b5aa
Rafael Correia <rafaelmcdono@gmail.com>
2023-12-15 09:39:58 +0000

    feature(no-jira): Adding Open API Specification.

--- src/main/java/com/rmc/currency_converter/dto/ConversionResultDTO.java
+++ src/main/java/com/rmc/currency_converter/dto/ConversionResultDTO.java
@@ -1,5 +1,6 @@
 package com.rmc.currency_converter.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConversionResultDTO {
+
+    @Schema(example = "EUR",
+            description = "Currency from which the conversion was made.")
     private String from;
+
+    @Schema(example = "10.50",
+            description = "Amount converted.")
     private Double amount;
+
+    @Schema(example = "USD",
+            description = "Currency to which the conversion was made.")
     private String to;
+
+    @Schema(example = "11.50",
+            description = "Result of the conversion.")
     private Double result;
 }
--- src/main/java/com/rmc/currency_converter/dto/RequestConversionDTO.java
+++ src/main/java/com/rmc/currency_converter/dto/RequestConversionDTO.java
@@ -1,5 +1,6 @@
 package com.rmc.currency_converter.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,14 +16,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestConversionDTO {
 
+    @Schema(example = "EUR",
+            description = "Currency from which the conversion will be made.")
     @NotBlank(message = "You must specify a currency. Ex: EUR")
     @Pattern(regexp = "^[a-zA-Z]{3}$", message = "The field must contain exactly 3 alphabetic characters.")
     private String from;
 
+    @Schema(example = "USD",
+            description = "Currency to which the conversion will be made.")
     @NotBlank(message = "You must specify a currency. Ex: EUR")
     @Pattern(regexp = "^[a-zA-Z]{3}$", message = "The field must contain exactly 3 alphabetic characters.")
     private String to;
 
+    @Schema(example = "10.50",
+            description = "Amount to be converted.")
     @NotNull(message = "You must specify at least one amount. Ex: 10.50")
     @Min(value = 0, message = "The field must be a positive number.")
     private Double amount;
